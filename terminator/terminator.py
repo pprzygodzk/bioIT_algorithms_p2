34300d9ff4379fe5269e9116b113e18619aaa02a
pprzygodzk <48103797+pprzygodzk@users.noreply.github.com>
2019-08-11 01:09:04 +0200

    Update terminator.py

--- terminator/terminator.py
+++ terminator/terminator.py
@@ -100,7 +100,7 @@ def GibbsFreeEnergy(t):
     return G
 
 def ShowTerminators(seq, terminators):
-    """shows graphically how a terminator t looks like"""
+    """shows graphically how each terminator from the list of terminators looks like"""
     
     ts = seq.replace("T", "U")
     l = lambda x: (x[3][0]-1)-x[1][1]
